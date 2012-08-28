8f042cd381777a5bd22efb47b152503bb2eaf3e2
mrclick <mr.click@goreclan.net>
2012-08-28 15:39:35 +0200

    Fixed sql script

--- extplugins/sql/jumper.sql
+++ extplugins/sql/jumper.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `maps_records` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `client_id` int(10) NOT NULL,
-  `map` varchar(32) NOT NULL,
-  `time` int(10) NOT NULL,
-  `time_add` int(10) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `client_id` int(10) unsigned NOT NULL,
+  `map` varchar(64) NOT NULL,
+  `time` int(10) unsigned NOT NULL,
+  `time_add` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
